fe8c1430c5906c2412279913109de8d2865ecf43
Liviu Preda <liviupreda@gmail.com>
2019-09-18 09:59:45 +0300

    define controllers and data structure

---
+++ app.js
@@ -0,0 +1,50 @@
+// Local Storage controller
+
+// Item controller
+const ItemCtrl = (function() {
+  // Item constructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+
+  // Data structure / State
+  // Data is private, we use the module pattern
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 900 },
+      { id: 1, name: 'Cookie', calories: 400 },
+      { id: 2, name: 'Eggs', calories: 300 }
+    ],
+    // when we click the update icon next to an item, the item will become the
+    // current item and will be put in the form to be updated
+    currentItem: null,
+    totalCalories: 0
+  };
+
+  // Public methods
+  return {
+    logData: function() {
+      return data;
+    }
+  };
+})();
+
+// UI controller
+const UICtrl = (function() {
+  // Public methods
+})();
+
+// App controller
+const App = (function(ItemCtrl, UICtrl) {
+  // Public methods
+  return {
+    init: function() {
+      console.log('Initializing app...');
+    }
+  };
+})(ItemCtrl, UICtrl);
+
+// Initialize app (edit state is clear, call getItems method)
+App.init();
